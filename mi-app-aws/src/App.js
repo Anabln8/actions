2bcdc3583a1216e3bf4f5654b1644ea28d320b88
Anabln8 <anabln26@gmail.com>
2025-07-28 19:29:58 +0200

    first commit

---
+++ mi-app-aws/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Mi App desplegada en AWS S3</h1>
+      <p>Bienvenido a mi aplicación React</p>
+    </div>
+  );
+}
+
+export default App;
